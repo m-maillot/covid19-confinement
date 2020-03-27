e898b0ccf90af5e66e72b9467749a010b1337e42
Martial Maillot <martial.maillot@gmail.com>
2020-03-27 13:36:06 +0100

    Updates

---
+++ precache-manifest.2a2a0be9b4c2e2a1badaf5dbd4547666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d16e54c697d2c79ffdde08ee9717382",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21233269185e5c81f53d",
+    "url": "/static/css/main.edc36af0.chunk.css"
+  },
+  {
+    "revision": "7236a211f3a06530719c",
+    "url": "/static/js/2.e91ad381.chunk.js"
+  },
+  {
+    "revision": "be0b74659518ec817941c420332039fd",
+    "url": "/static/js/2.e91ad381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21233269185e5c81f53d",
+    "url": "/static/js/main.0a053de7.chunk.js"
+  },
+  {
+    "revision": "061c892c5c1e4b62b9a6",
+    "url": "/static/js/runtime-main.ed7d1669.js"
+  }
+]);
